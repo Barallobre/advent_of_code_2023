ad15c3f56d94be81eff67c2bd81d060599900a33
Barallobre <cristianbarallobrerua@gmail.com>
2023-12-01 21:54:29 +0100

    Primera parte

---
+++ advent_of_code_23/DataConverter_day1.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace advent_of_code_23
+{
+    internal class DataConverter_day1
+    {
+        public static List<int> getData(string data)
+        {
+            List<int> inputList = new List<int>();
+            var word = new StringBuilder();
+            var words = data.Replace("\r\n", " ");
+
+            foreach (Char c in words)
+            {
+                if (c == ' ')
+                {
+                    if (word.Length == 1)
+                    {
+                        string reducedNumber = String.Format("{0}{1}", word.ToString()[0], word.ToString()[0]);
+                        inputList.Add(Int32.Parse(reducedNumber));
+                        word = new StringBuilder();
+                        continue;
+                    }
+                    else if (word.Length > 2)
+                    {
+                        string reducedNumber = String.Format("{0}{1}", word.ToString()[0], word[word.Length - 1]);
+                        inputList.Add(Int32.Parse(reducedNumber));
+                        word = new StringBuilder();
+                        continue;
+                    }
+                    inputList.Add(Int32.Parse(word.ToString()));
+                    word = new StringBuilder();
+                    continue;
+                }
+                if (Char.IsDigit(c))
+                {
+                    word.Append(c);
+                }
+                
+            }
+            return inputList;
+        }        
+    }
+}
--- advent_of_code_23/Program.cs
+++ advent_of_code_23/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using advent_of_code_23;
+
+string data = "";
+
+var dataConverted = DataConverter_day1.getData(data);
+
+long sum = 0;
+
+foreach(var item in dataConverted)
+{
+    sum = sum + item;
+}
+
+Console.WriteLine(sum);
+
+
+
