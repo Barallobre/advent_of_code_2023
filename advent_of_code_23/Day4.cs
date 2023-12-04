0fd0354559e16887cefe42db1bb5f04e0ab19dfa
Barallobre <cristianbarallobrerua@gmail.com>
2023-12-04 15:50:50 +0100

    day 4 numbers mapping

---
+++ advent_of_code_23/Day4.cs
@@ -0,0 +1,63 @@
+﻿using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace advent_of_code_23
+{
+    internal class Day4
+    {
+        public static void Solution(List<string> lines) 
+        {
+            string regexPattern = @"[1-9][0-9]{0,2}: ";
+            
+            List<string> numbersGame = new List<string>();
+            List<List<string>> allNumbersGame = new List<List<string>>();
+
+            List<string> myNumbers = new List<string>();
+            List<List<string>> allMyNumbers = new List<List<string>>();
+
+            StringBuilder numberBuffer = new StringBuilder();
+  
+            foreach (string line in lines)
+            {
+                string cleanedLine = Regex.Replace(line, regexPattern, "").Replace("Card ","");
+                
+                string[] twoLines = cleanedLine.Split('|');
+
+                foreach (Char c in twoLines[0])
+                {
+                    if (Char.IsDigit(c))
+                    {
+                        numberBuffer.Append(c);                 
+                    }
+                    else if (numberBuffer.Length != 0)
+                    {
+                        numbersGame.Add(numberBuffer.ToString());
+                        numberBuffer = new StringBuilder();
+                    }
+                }
+                allNumbersGame.Add(numbersGame);
+                numbersGame = new List<string>();
+                numberBuffer = new StringBuilder();
+           
+                foreach (Char c in twoLines[1])
+                {
+                    if (Char.IsDigit(c))
+                    {
+                        numberBuffer.Append(c);
+                    }
+                    else if (numberBuffer.Length != 0)
+                    {
+                        myNumbers.Add(numberBuffer.ToString());
+                        numberBuffer = new StringBuilder();
+                    }
+                }
+                if (numberBuffer.Length != 0) { myNumbers.Add(numberBuffer.ToString()); }
+                allMyNumbers.Add(myNumbers);
+                myNumbers = new List<string>();
+                numberBuffer = new StringBuilder();
+
+            }
+        }        
+    }
+}
--- advent_of_code_23/Program.cs
+++ advent_of_code_23/Program.cs
@@ -1,13 +1,13 @@
 ﻿using advent_of_code_23;
 
-string file = @"\advent_of_code_2023\advent_of_code_23\input.txt";
+string file = @"\advent_of_code_2023\advent_of_code_23\prueba.txt";
 
 
 List<string> lines = new List<string>();
 
 FileReader.Reader(file, ref lines);
 
-Day3_2.Solution(lines);
+Day4.Solution(lines);
 
 
 
