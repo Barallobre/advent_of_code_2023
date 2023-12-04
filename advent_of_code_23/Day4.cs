17f1af7218744fa4ef4c395f3e68dd62c8c8d488
Barallobre <cristianbarallobrerua@gmail.com>
2023-12-04 16:25:53 +0100

    day 4 part 1 solved

--- advent_of_code_23/Day4.cs
+++ advent_of_code_23/Day4.cs
@@ -1,14 +1,14 @@
-﻿using System.Drawing;
-using System.Text;
+﻿using System.Text;
 using System.Text.RegularExpressions;
 
 namespace advent_of_code_23
 {
     internal class Day4
     {
+        
         public static void Solution(List<string> lines) 
         {
-            string regexPattern = @"[1-9][0-9]{0,2}: ";
+            
             
             List<string> numbersGame = new List<string>();
             List<List<string>> allNumbersGame = new List<List<string>>();
@@ -17,18 +17,49 @@ namespace advent_of_code_23
             List<List<string>> allMyNumbers = new List<List<string>>();
 
             StringBuilder numberBuffer = new StringBuilder();
-  
+            
+            (allNumbersGame, allMyNumbers) = MappingNumbers(lines, numbersGame, allNumbersGame, myNumbers, allMyNumbers, numberBuffer);
+
+            double power = 0;
+            double total = 0;
+            double sum = 0;
+            for (int i = 0; i < allMyNumbers.Count; i++)
+            {
+                foreach (var numberPerCard in allNumbersGame[i])
+                {
+                    foreach (var myNumberPerCard in allMyNumbers[i])
+                    {
+                        if (numberPerCard ==myNumberPerCard)
+                        {
+                            power++;
+                        }
+                    }
+                }
+                sum = power == 0 ? 0 : Math.Pow(2, power) / 2;
+                total += sum;
+                power = 0;
+            }
+
+            Console.WriteLine(total);           
+        }   
+        
+        private static (List<List<string>>, List<List<string>>) MappingNumbers(
+            List<string> lines, List<string> numbersGame, List<List<string>> allNumbersGame,
+            List<string> myNumbers, List<List<string>> allMyNumbers, StringBuilder numberBuffer)
+        {
+            string regexPattern = @"[1-9][0-9]{0,2}: ";
+
             foreach (string line in lines)
             {
-                string cleanedLine = Regex.Replace(line, regexPattern, "").Replace("Card ","");
-                
+                string cleanedLine = Regex.Replace(line, regexPattern, "").Replace("Card ", "");
+
                 string[] twoLines = cleanedLine.Split('|');
 
                 foreach (Char c in twoLines[0])
                 {
                     if (Char.IsDigit(c))
                     {
-                        numberBuffer.Append(c);                 
+                        numberBuffer.Append(c);
                     }
                     else if (numberBuffer.Length != 0)
                     {
@@ -39,7 +70,7 @@ namespace advent_of_code_23
                 allNumbersGame.Add(numbersGame);
                 numbersGame = new List<string>();
                 numberBuffer = new StringBuilder();
-           
+
                 foreach (Char c in twoLines[1])
                 {
                     if (Char.IsDigit(c))
@@ -58,6 +89,8 @@ namespace advent_of_code_23
                 numberBuffer = new StringBuilder();
 
             }
-        }        
+
+            return (allNumbersGame, allMyNumbers);
+        }
     }
 }
