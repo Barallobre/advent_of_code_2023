b48d9f53ca662d7acb62e71a612294cc00f1736e
Barallobre <cristianbarallobrerua@gmail.com>
2023-12-09 19:10:04 +0100

    Day 7 part 1 solved

--- advent_of_code_23/Day7.cs
+++ advent_of_code_23/Day7.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -10,7 +12,7 @@ namespace advent_of_code_23
         public static void Solution(List<string> lines)
         {
             StringBuilder numberBuffer = new StringBuilder();
-            List<(string, int)> hands = new List<(string, int)>();
+            List<(string, int, int)> hands = new List<(string, int, int)>();
             List<string> handsBids = new List<string>();
 
             foreach (string line in lines)
@@ -21,28 +23,134 @@ namespace advent_of_code_23
 
             hands = GroupHandsBids(handsBids);
 
-            List<(string, int)> fiveOfAKind = new List<(string, int)>();
-            List<(string, int)> fourOFAKind = new List<(string, int)>();
-            List<(string, int)> fullHouse = new List<(string, int)>();
-            List<(string, int)> threeOfAKind = new List<(string, int)>();
-            List<(string, int)> twoPair = new List<(string, int)>();
-            List<(string, int)> onePair = new List<(string, int)>();
-            List<(string, int)> hightCard = new List<(string, int)>();
+            List<(string, int, int)> fiveOfAKind = new List<(string, int, int)>();
+            List<(string, int, int)> fourOFAKind = new List<(string, int, int)>();
+            List<(string, int, int)> fullHouse = new List<(string, int, int)>();
+            List<(string, int, int)> threeOfAKind = new List<(string, int, int)>();
+            List<(string, int, int)> twoPair = new List<(string, int, int)>();
+            List<(string, int, int)> onePair = new List<(string, int, int)>();
+            List<(string, int, int)> hightCard = new List<(string, int, int)>();
 
 
             HandsByType(hands, fiveOfAKind, fourOFAKind, fullHouse, threeOfAKind, twoPair, onePair, hightCard);
 
+            string[] ranks = {"A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2" };
+
+            OrderByRanks(fiveOfAKind, fourOFAKind, fullHouse, threeOfAKind, twoPair, onePair, hightCard, ranks);
 
         }
+
+        private static void OrderByRanks(
+            List<(string, int, int)> fiveOfAKind,
+            List<(string, int, int)> fourOFAKind,
+            List<(string, int, int)> fullHouse,
+            List<(string, int, int)> threeOfAKind,
+            List<(string, int, int)> twoPair,
+            List<(string, int, int)> onePair,
+            List<(string, int, int)> hightCard,
+            string[] ranks)
+        {
+            List<(string, int)> fiveOfAKindCopy = new List<(string, int)> ();
+            List<(string, int)> fourOFAKindCopy = new List<(string, int)> ();
+            List<(string, int)> fullHouseCopy = new List<(string, int)> ();
+            List<(string, int)> threeOfAKindCopy = new List<(string, int)> ();
+            List<(string, int)> twoPairCopy = new List<(string, int)> ();
+            List<(string, int)> onePairCopy = new List<(string, int)> ();
+            List<(string, int)> hightCardCopy = new List<(string, int)> ();
+
+            TranslatorToNumbers(fiveOfAKind, fiveOfAKindCopy, ranks);
+            TranslatorToNumbers(fourOFAKind, fourOFAKindCopy, ranks);
+            TranslatorToNumbers(fullHouse, fullHouseCopy, ranks);
+            TranslatorToNumbers(threeOfAKind, threeOfAKindCopy, ranks);
+            TranslatorToNumbers(twoPair, twoPairCopy, ranks);
+            TranslatorToNumbers(onePair, onePairCopy, ranks);
+            TranslatorToNumbers(hightCard, hightCardCopy, ranks);
+
+            OrderNumbersByRank(fiveOfAKindCopy, fiveOfAKind);
+            OrderNumbersByRank(fourOFAKindCopy, fourOFAKind);
+            OrderNumbersByRank(fullHouseCopy, fullHouse);
+            OrderNumbersByRank(threeOfAKindCopy, threeOfAKind);
+            OrderNumbersByRank(twoPairCopy, twoPair);
+            OrderNumbersByRank(onePairCopy, onePair);
+            OrderNumbersByRank(hightCardCopy, hightCard);
+
+            List<(string, int, int)> orderedRanks = new List<(string, int, int)>();
+
+            FinalOrder(fiveOfAKind, orderedRanks);
+            FinalOrder(fourOFAKind, orderedRanks);
+            FinalOrder(fullHouse, orderedRanks);
+            FinalOrder(threeOfAKind, orderedRanks);
+            FinalOrder(twoPair, orderedRanks);
+            FinalOrder(onePair, orderedRanks);
+            FinalOrder(hightCard, orderedRanks);    
+  
+
+            long total = 0;
+            int multiply = 1;
+            for (int i = orderedRanks.Count - 1; i >= 0; i--)
+            {
+                total += orderedRanks[i].Item2 * multiply;
+                multiply++;
+            }
+            Console.WriteLine(total);
+        }
+
+        private static void FinalOrder(List<(string, int, int)> mainList, List<(string, int, int)> orderedRanks)
+        {
+            mainList.Sort((x, y) => x.Item3.CompareTo(y.Item3));
+            foreach (var item in mainList)
+            {
+                orderedRanks.Add(item);
+            }
+        }
+
+        private static void OrderNumbersByRank(List<(string, int)> copyOfListOfRanks, List<(string, int, int)> mainList)
+        {
+            
+            for (int k = 0; k < copyOfListOfRanks.Count; k++)
+            {
+                int hexToNumber = int.Parse(copyOfListOfRanks[k].Item1, System.Globalization.NumberStyles.HexNumber);
+                Tuple<string, int, int> tup = new Tuple<string, int, int>(mainList[k].Item1, mainList[k].Item2, hexToNumber);
+                //mainList.RemoveAt(k);
+                mainList[k] = ((tup.Item1, tup.Item2, tup.Item3));
+
+            }
+        }
+
+        private static void TranslatorToNumbers(List<(string, int, int)>  ranksOfHands, 
+            List<(string, int)> ranksOfHandsCopy, string[] ranks)
+        {
+            foreach (var hand in ranksOfHands)
+            {
+                StringBuilder handCopy = new StringBuilder();
+
+                foreach (var c in hand.Item1)
+                {
+                    for (int i = 0; i < ranks.Length; i++)
+                    {
+                        if (c.ToString() == ranks[i].ToString())
+                        {
+                            
+                            string hexValue = i.ToString("X");
+                            handCopy.Append(hexValue);
+                        }
+                    }
+                }
+                ranksOfHandsCopy.Add((handCopy.ToString(), hand.Item2));
+            }
+        }
+
+
+
         private static void HandsByType(
-            List<(string, int)> hands,
-            List<(string, int)> fiveOfAKind,
-            List<(string, int)> fourOFAKind,
-            List<(string, int)> fullHouse,
-            List<(string, int)> threeOfAKind,
-            List<(string, int)> twoPair,
-            List<(string, int)> onePair,
-            List<(string, int)> hightCard)
+            List<(string, int, int)> hands,
+            List<(string, int, int)> fiveOfAKind,
+            List<(string, int, int)> fourOFAKind,
+            List<(string, int, int)> fullHouse,
+            List<(string, int, int)> threeOfAKind,
+            List<(string, int, int)> twoPair,
+            List<(string, int, int)> onePair,
+            List<(string, int, int)> hightCard)
         { 
 
             List<int> repetitions = new List<int>();
@@ -72,31 +180,31 @@ namespace advent_of_code_23
 
                 if (sumTotal == 25)
                 {
-                    fiveOfAKind.Add((hand.Item1, hand.Item2));
+                    fiveOfAKind.Add((hand.Item1, hand.Item2, 0));
                 }
                 if (sumTotal == 17)
                 {
-                    fourOFAKind.Add((hand.Item1, hand.Item2));
+                    fourOFAKind.Add((hand.Item1, hand.Item2, 0));
                 }
                 if (sumTotal == 13)
                 {
-                    fullHouse.Add((hand.Item1, hand.Item2));
+                    fullHouse.Add((hand.Item1, hand.Item2, 0));
                 }
                 if (sumTotal == 11)
                 {
-                    threeOfAKind.Add((hand.Item1, hand.Item2));
+                    threeOfAKind.Add((hand.Item1, hand.Item2, 0));
                 }
                 if (sumTotal == 9)
                 {
-                    twoPair.Add((hand.Item1, hand.Item2));
+                    twoPair.Add((hand.Item1, hand.Item2, 0));
                 }
                 if (sumTotal == 7)
                 {
-                    onePair.Add((hand.Item1, hand.Item2));
+                    onePair.Add((hand.Item1, hand.Item2, 0));
                 }
                 if (sumTotal == 5)
                 {
-                    hightCard.Add((hand.Item1, hand.Item2));
+                    hightCard.Add((hand.Item1, hand.Item2, 0));
                 }
             }
 
@@ -117,13 +225,13 @@ namespace advent_of_code_23
             if (letterDigitBuffer.Length != 0) { hands.Add(letterDigitBuffer.ToString()); }
         }
 
-        private static List<(string, int)> GroupHandsBids(List<string> handsBids)
+        private static List<(string, int, int)> GroupHandsBids(List<string> handsBids)
         {
             string hand;
             int bid = 0;
 
             Tuple<string, int> tup = new Tuple<string, int>(string.Empty, 0);
-            List<(string, int)> numbersByTypeInTupleList = new List<(string, int)>();
+            List<(string, int, int)> numbersByTypeInTupleList = new List<(string, int, int)>();
 
             for (int i = 0; i < handsBids.Count; i ++)
             {
@@ -136,7 +244,7 @@ namespace advent_of_code_23
 
 
                 tup = new Tuple<string, int>(hand, bid);
-                numbersByTypeInTupleList.Add((tup.Item1, tup.Item2));
+                numbersByTypeInTupleList.Add((tup.Item1, tup.Item2, 0));
                 
             }
 
